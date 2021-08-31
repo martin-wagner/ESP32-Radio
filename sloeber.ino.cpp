70e7b73ca8fe7c04554bc1b32fbd2bd7dc73ed08
Martin Wagner <martin.wagner@kaltensondheim.de>
2021-08-31 13:42:20 +0200

    Fix Build

---
+++ sloeber.ino.cpp
@@ -0,0 +1,13 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2021-08-31 13:32:24
+
+#include "Arduino.h"
+
+
+
+#include "ESP32-Radio.ino"
+
+#endif

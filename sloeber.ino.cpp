b93cc4c0565bb2c3f9edc72e4c97ee56d584e892
Martin Wagner <martin.wagner@kaltensondheim.de>
2022-02-12 13:46:56 +0100

    Add test build to eclise -- now indexes testing framework

--- sloeber.ino.cpp
+++ sloeber.ino.cpp
@@ -2,7 +2,7 @@
 //This is a automatic generated file
 //Please do not modify this file
 //If you touch this file your change will be overwritten during the next build
-//This file has been generated on 2021-08-31 13:32:24
+//This file has been generated on 2022-02-12 13:26:30
 
 #include "Arduino.h"
 
